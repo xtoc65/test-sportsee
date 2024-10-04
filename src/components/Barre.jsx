0c3d4a724f3a5ae4846efc7d12994752c8bbd846
Flora Paul <flora.p3108@gmail.com>
2024-10-04 19:44:40 +0200

    dinamisation du diagrame en barre

--- src/components/Barre.jsx
+++ src/components/Barre.jsx
@@ -1,46 +1,8 @@
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-// Données de l'utilisateur avec userId: 12
-const userActivityData = [
-  {
-    "day": "2020-07-01",
-    "kilogram": 70,
-    "calories": 240
-    },
-    {
-    "day": "2020-07-02",
-    "kilogram": 69,
-    "calories": 220
-    },
-    {
-    "day": "2020-07-03",
-    "kilogram": 70,
-    "calories": 280
-    },
-    {
-    "day": "2020-07-04",
-    "kilogram": 70,
-    "calories": 500
-    },
-    {
-    "day": "2020-07-05",
-    "kilogram": 69,
-    "calories": 160
-    },
-    {
-    "day": "2020-07-06",
-    "kilogram": 69,
-    "calories": 162
-    },
-    {
-    "day": "2020-07-07",
-    "kilogram": 69,
-    "calories": 390
-    }
-];
 
-function Barre() {
+function Barre({activities}) {
   return (
     <div style={{ width: '100%', height: 400 }}>
       {/* Conteneur du titre et de la légende */}
@@ -50,7 +12,7 @@ function Barre() {
       </div>
       <ResponsiveContainer width="100%" height={400}>
       <BarChart
-        data={userActivityData}
+        data={activities}
         margin={{
           top: 20, right: 30, left: 20, bottom: 5,
         }}
@@ -69,9 +31,9 @@ function Barre() {
           <Tooltip />
           <Legend verticalAlign="top" align="right"  wrapperStyle={{ padding: '10px' }} />
           {/* Barre pour le poids en kilogrammes */}
-          <Bar dataKey="kilogram" fill="#282D30" name="Poids (kg)"  radius={[10, 10, 0, 0]} barSize={15}/>
+          <Bar dataKey="kilogram" fill="#282D30" name="Poids (kg)" radius={[10, 10, 0, 0]} barSize={15}/>
           {/* Barre pour les calories brûlées */}
-          <Bar dataKey="calories" fill="#E60000" name="Calories brûlées (KCal)"  radius={[10, 10, 0, 0]} barSize={15}/>
+          <Bar dataKey="calories" fill="#E60000" name="Calories brûlées (KCal)" radius={[10, 10, 0, 0]} barSize={15}/>
         </BarChart>
       </ResponsiveContainer>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Barre from "../components/Barre";
 import Courbe from "../components/Courbe"
 import RadarChart from "../components/RadarChart";
@@ -7,18 +8,31 @@ import Aside from "../components/Aside";
 import "../assets/styles/home.css"
 
 function Home() {
+    const [userActivityData, modifierUserActivity] = useState([1]) 
+    function handleUserActivity(activity){
+        modifierUserActivity(activity.data.sessions)
+    }
+    function handleError(){
+        console.log(("Erreur"));
+    }
+    useEffect(() => {
+    fetch("http://localhost:3000/user/18/activity")
+        .then(response => response.json())
+        .then(handleUserActivity)
+        .catch(handleError) 
+    },[])
     return (
         <div>
             <Aside />
             <main>
                 <div>
-                    <h1>Bonjour</h1>
+                    <h1>Bonjour Toto</h1>
                     <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
                 </div>
                 <section>
                     <article>
                         <div className="chart full-width">
-                        <Barre />
+                        <Barre activities={userActivityData}/>
                         </div>
                         {/* Conteneur pour les trois graphiques sur la même ligne */}
                         <div className="charts-row">
